f2212f9f47dcab146de8fa2fd5f9cdca9d01b2b5
RobertoFigueroa <fig18306@uvg.edu.gt>
2021-05-27 16:13:01 -0600

    [REF] new folder for MCTS implementation

--- Hoppers2.0/minimax.py
+++ Hoppers2.0/minimax.py
@@ -4,7 +4,6 @@ from path import NodePath
 import numpy as np
 import time
 import math
-from collections import defaultdict
 
 
 class Minimax:
@@ -20,7 +19,6 @@ class Minimax:
         self.prunes = 0
         self.boards = 0
         self.alphaBeta = alphaBeta
-        self.path = []
 
     def clearPath(self):
         self.path = []
@@ -305,105 +303,4 @@ class Minimax:
 
         return_node.set_value(value)
         return return_node, best_move
-
-
-
-class MonteCarloTreeSearchNode():
-    def __init__(self, state, parent=None, parent_action=None):
-        self.state = state #board
-        self.parent = parent
-        self.parent_action = parent_action
-        self.children = []
-        self._number_of_visits = 0
-        self._results = defaultdict(int)
-        self._results[1] = 0
-        self._results[-1] = 0
-        self._untried_actions = None
-        self._untried_actions = self.untried_actions()
-
-    def untried_actions(self):
-        self._untried_actions = self.state.get_legal_moves()
-        return self._untried_actions
-
-    def q(self):
-        wins = self._results[1]
-        loses = self._results[-1]
-        return wins - loses
-
-    def n(self):
-        return self._number_of_visits
-
-
-    def expand(self):
-	
-        action = self._untried_actions.pop()
-        next_state = self.state.move_piece(action)
-        child_node = MonteCarloTreeSearchNode(
-            next_state, parent=self, parent_action=action)
-
-        self.children.append(child_node)
-        return child_node 
-
-    def is_terminal_node(self):
-        return self.state.detectWin() #checkWin
-
-    
-    def rollout(self):
-        current_rollout_state = self.state
-        
-        while not current_rollout_state.detectWin():
-            
-            possible_moves = current_rollout_state.get_legal_moves()
-            
-            action = self.rollout_policy(possible_moves)
-            current_rollout_state = current_rollout_state.move(action)
-        return current_rollout_state.detectWin()
-
-    def backpropagate(self, result):
-        self._number_of_visits += 1.
-        self._results[result] += 1.
-        if self.parent:
-            self.parent.backpropagate(result)
-
-    def is_fully_expanded(self):
-        return len(self._untried_actions) == 0
-
-    def best_child(self, c_param=0.1):
-        
-        choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
-        return self.children[np.argmax(choices_weights)]
-
-    def rollout_policy(self, possible_moves):
-        
-        return possible_moves[np.random.randint(len(possible_moves))]
-
-
-    def _tree_policy(self):
-
-        current_node = self
-        while not current_node.detectWin():
-            
-            if not current_node.is_fully_expanded():
-                return current_node.expand()
-            else:
-                current_node = current_node.best_child()
-        return current_node
-
-
-    def best_action(self):
-        simulation_no = 100
-        
-        
-        for i in range(simulation_no):
-            
-            v = self._tree_policy()
-            reward = v.rollout()
-            v.backpropagate(reward)
-        
-        return self.best_child(c_param=0.)
-
-    
-
-
-                    
 
